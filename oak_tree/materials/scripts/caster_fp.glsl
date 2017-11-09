7c00357086b94440698d28e847fc7ac62d684d01
Ian Chen <ichen@osrfoundation.org>
2017-11-09 11:53:07 -0800

    add shadow caster shaders that work with textures with alpha channels

---
+++ oak_tree/materials/scripts/caster_fp.glsl
@@ -0,0 +1,20 @@
+// uniform vec4 depth_range;
+varying vec4 vertex_depth;
+
+uniform sampler2D tex;
+varying vec2 uv;
+
+void main()
+{
+  if (texture2D(tex, uv).a < 0.5)
+  {
+    discard;
+  }
+
+  float depth = (vertex_depth.z) / vertex_depth.w;
+
+  // Linear
+  // float depth = (vertex_depth.z - depth_range.x) / depth_range.w;
+
+  gl_FragColor = vec4(depth, depth, depth, 1.0);
+}
